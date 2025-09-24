a06b4ace1fa3faef0c2a1ff53f1ee89e7ba07f63
ZAINIL MUSTHOFA RADAM <zenradam@gmail.com>
2025-09-24 20:25:04 +0800

    zainil/add initial C program files for Praktikum assignments

---
+++ C/PRAK002-251081731003-ZAINIL_MUSTHOFA_RADAM.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+    printf("Selamat Pagi, Zainil Musthofa Radam\n");
+    printf("Selamat Siang, Zainil Musthofa Radam\n");
+    printf("Selamat Malam, Zainil Musthofa Radam\n");
+   
+    return 0;
+}
---
+++ C/PRAK004-251081731003-ZAINIL_MUSTHOFA_RADAM.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("############################\n");
+    printf("#                          #\n");
+    printf("#   Zainil Musthofa Radam  #\n");
+    printf("#       2510817310015      #\n");
+    printf("#                          #\n");
+    printf("############################\n");
+    return 0;
+}
