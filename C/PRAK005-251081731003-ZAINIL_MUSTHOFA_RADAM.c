c7681c6fd30386f34f027655d825a4f74fd48469
ZAINIL MUSTHOFA RADAM <zenradam@gmail.com>
2025-09-24 20:28:04 +0800

    zainil/fix formatting in PRAK005-251081731003-ZAINIL_MUSTHOFA_RADAM.c

--- C/PRAK005-251081731003-ZAINIL_MUSTHOFA_RADAM.c
+++ C/PRAK005-251081731003-ZAINIL_MUSTHOFA_RADAM.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-printf("######   #     \n");
-printf("    #    #     \n");
-printf("   #     #     \n");
-printf("  #      #     \n");
-printf(" #       #     \n");
-printf("######   ######\n");
+    printf("######   #     \n");
+    printf("    #    #     \n");
+    printf("   #     #     \n");
+    printf("  #      #     \n");
+    printf(" #       #     \n");
+    printf("######   ######\n");
 
     return 0;
 }
